14ff0a773265f897dfe751d01a2e1c5db81fd6cd
Scott Doxey <neogeek0101@gmail.com>
2021-03-14 01:31:07 -0500

    Version bump.

--- build-unity-android-plugin.rb
+++ build-unity-android-plugin.rb
@@ -1,8 +1,8 @@
 class BuildUnityAndroidPlugin < Formula
   desc "Build Android plugins for Unity without needing to setup an Android project."
   homepage "https://github.com/neogeek/build-unity-android-plugin"
-  url "https://github.com/neogeek/build-unity-android-plugin/archive/v1.1.0.tar.gz"
-  sha256 "4c783375673ab80edc1b3c654b798b61165acab36b234a5fba6ecd5ec44c6d5d"
+  url "https://github.com/neogeek/build-unity-android-plugin/archive/v1.2.0.tar.gz"
+  sha256 "edd95a3024248b2db7cae7a0f70b05aeba14d9fb248d565c836646a79fe6e57a"
 
   def install
     bin.install "bin/build-unity-android-plugin"
